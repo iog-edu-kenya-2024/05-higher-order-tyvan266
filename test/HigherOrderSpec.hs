248c6bfc9cbe6791d9ba6cbdddbc7186e589cba6
Lars Brünjes <brunjlar@gmail.com>
2020-07-23 17:02:29 +0200

    prepared assignment

---
+++ test/HigherOrderSpec.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module HigherOrderSpec
+    ( spec
+    ) where
+
+import           Test.Hspec
+import           Test.QuickCheck
+
+import           HigherOrder
+
+spec :: Spec
+spec = return ()
